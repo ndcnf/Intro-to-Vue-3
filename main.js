1fa29cf3736b5045f5e7b24e028406f190e54f57
Nadia <nadia.chennaf@gmail.com>
2023-06-07 08:16:57 +0200

    first lesson

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+//   data: function() { // maniere d'ecrire avant ES6
+    data() { // ES6 shorthand
+        return {
+            product: 'Socks',
+            description: 'Some quality ones, premium ones even. No repeat.'
+        }
+      }
+})
