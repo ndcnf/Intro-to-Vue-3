71cde18c67a6834e63c03bdea1342a56d7a76491
nchennaf <nchennaf@student.42lausanne.ch>
2023-06-08 11:21:39 +0200

    ex9 ok

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -5,17 +5,14 @@ app.component('product-details', {
 			required: true
 		}
 	},
+	template:
+	/*html*/
+	`<ul>
+	<li v-for="detail in details">{{ detail }}</li>
+	</ul>`,
 	data() {
 		return {
 			details: ['50% cotton', '30% wool', '20% polyester']
 		}
 	},
-	computed: {
-		details() {
-			<ul>
-			<li v-for="detail in details">{{ detail }}</li>
-			</ul>
-			return
-		}
-	}
 })
